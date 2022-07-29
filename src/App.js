321a49b6e00ef583f82207b5c5b20a918aa800ac
Holybless <adrielfa82@gmail.com>
2022-07-29 01:21:31 -0300

    adding route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import Directory from './components/directory/directory.component';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Home from './routes/home/home.component';
+import QuizOne from './components/quiz-one/quiz-one.component';
+import Navigation from './routes/navigation/navigation.component';
 
 function App() {
   return (
-    <div className="App">
-      <Directory/>
+    <div className='app'>
+      <Routes>
+        <Route path='/' element = {<Navigation/>}>
+          <Route index element={<Home/>} />
+          <Route path='quiz-one' element={<QuizOne/>} />         
+        </Route>
+      </Routes>
     </div>
   );
 };
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,12 +1,14 @@
 import QuizOne from "../quiz-one/quiz-one.component";
 import QuizTwo from "../quiz-two/quiz-two.component";
+import { Link } from 'react-router-dom'
 
 const Directory = () => {
     return (
         <div>
-            <h1>Pagina de quizes de prueba</h1>
-            <QuizOne/>
-            <QuizTwo/>
+            <h1>Test quizes page</h1>
+            <Link to="quiz-one">
+                <h2>Go to quiz one</h2>
+            </Link>
         </div>
     );
 };
